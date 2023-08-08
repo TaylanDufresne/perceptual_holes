0a56594063434d4b4b46d025d57e0dd41135601f
TaylanDufresne <taylan.dufresne@gmail.com>
2023-08-08 08:14:06 -0600

    added flight data

--- src/flight_tracking/fake_page.js
+++ src/flight_tracking/fake_page.js
@@ -14,6 +14,220 @@ let areas = {
   "LAX": [790, 498, 807, 513],
 }
 
+let flightData = {
+  "AA123": {
+    "departureTime": "10:00 AM",
+    "arrivalTime": "12:00 PM",
+    "departureAirport": "JFK",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Boeing 737"
+  },
+  "UA456": {
+    "departureTime": "1:00 PM",
+    "arrivalTime": "3:00 PM",
+    "departureAirport": "ORD",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Airbus A320"
+  },
+  "DL789": {
+    "departureTime": "6:00 AM",
+    "arrivalTime": "9:00 AM",
+    "departureAirport": "ATL",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Boeing 757"
+  },
+  "WN234": {
+    "departureTime": "8:00 PM",
+    "arrivalTime": "10:00 PM",
+    "departureAirport": "LAS",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Boeing 737"
+  },
+  "AS567": {
+    "departureTime": "11:00 AM",
+    "arrivalTime": "1:00 PM",
+    "departureAirport": "SEA",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Airbus A321"
+  },
+  "B6123": {
+    "departureTime": "2:00 PM",
+    "arrivalTime": "4:00 PM",
+    "departureAirport": "JFK",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Embraer E190"
+  },
+  "NK456": {
+    "departureTime": "5:00 PM",
+    "arrivalTime": "7:00 PM",
+    "departureAirport": "ORD",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Airbus A320"
+  },
+  "F9789": {
+    "departureTime": "9:00 AM",
+    "arrivalTime": "11:00 AM",
+    "departureAirport": "ATL",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Boeing 737"
+  },
+  "G4123": {
+    "departureTime": "3:00 PM",
+    "arrivalTime": "5:00 PM",
+    "departureAirport": "LAS",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Embraer E175"
+  },
+  "HA567": {
+    "departureTime": "7:00 AM",
+    "arrivalTime": "10:00 AM",
+    "departureAirport": "HNL",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Airbus A330"
+  },
+  "SY456": {
+    "departureTime": "4:00 PM",
+    "arrivalTime": "6:00 PM",
+    "departureAirport": "MSP",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Boeing 737"
+  },
+  "YX789": {
+    "departureTime": "12:00 PM",
+    "arrivalTime": "2:00 PM",
+    "departureAirport": "ORD",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Embraer E175"
+  },
+  "9E234": {
+    "departureTime": "6:00 AM",
+    "arrivalTime": "8:00 AM",
+    "departureAirport": "JFK",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Bombardier CRJ900"
+  },
+  "MQ567": {
+    "departureTime": "9:00 AM",
+    "arrivalTime": "11:00 AM",
+    "departureAirport": "ORD",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Embraer E175"
+  },
+  "OH456": {
+    "departureTime": "2:00 PM",
+    "arrivalTime": "4:00 PM",
+    "departureAirport": "CLT",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Bombardier CRJ700"
+  },
+  "YV789": {
+    "departureTime": "5:00 PM",
+    "arrivalTime": "7:00 PM",
+    "departureAirport": "IAD",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Embraer E175"
+  },
+  "CP123": {
+    "departureTime": "11:00 AM",
+    "arrivalTime": "1:00 PM",
+    "departureAirport": "PHX",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Embraer E175"
+  },
+  "ZW456": {
+    "departureTime": "3:00 PM",
+    "arrivalTime": "5:00 PM",
+    "departureAirport": "MKE",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Bombardier CRJ200"
+  },
+  "G7123": {
+    "departureTime": "8:00 AM",
+    "arrivalTime": "10:00 AM",
+    "departureAirport": "SFO",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Embraer E175"
+  },
+  "EV567": {
+    "departureTime": "1:00 PM",
+    "arrivalTime": "3:00 PM",
+    "departureAirport": "IAH",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Bombardier CRJ700"
+  },
+  "OO789": {
+    "departureTime": "7:00 AM",
+    "arrivalTime": "9:00 AM",
+    "departureAirport": "DEN",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Bombardier CRJ700"
+  },
+  "C5123": {
+    "departureTime": "12:00 PM",
+    "arrivalTime": "2:00 PM",
+    "departureAirport": "CLE",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Embraer E175"
+  },
+  "PT456": {
+    "departureTime": "4:00 PM",
+    "arrivalTime": "6:00 PM",
+    "departureAirport": "PHL",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Embraer E175"
+  },
+  "AX789": {
+    "departureTime": "10:00 AM",
+    "arrivalTime": "12:00 PM",
+    "departureAirport": "STL",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Embraer E175"
+  },
+  "ZW234": {
+    "departureTime": "5:00 PM",
+    "arrivalTime": "7:00 PM",
+    "departureAirport": "MKE",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Bombardier CRJ200"
+  },
+  "KS567": {
+    "departureTime": "3:00 PM",
+    "arrivalTime": "5:00 PM",
+    "departureAirport": "LAX",
+    "arrivalAirport": "ADQ",
+    "aircraftType": "Saab 340"
+  },
+  "EM456": {
+    "departureTime": "6:00 AM",
+    "arrivalTime": "8:00 AM",
+    "departureAirport": "LAX",
+    "arrivalAirport": "MFR",
+    "aircraftType": "Cessna 208"
+  },
+  "2O789": {
+    "departureTime": "9:00 AM",
+    "arrivalTime": "11:00 AM",
+    "departureAirport": "HNL",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Bombardier Q400"
+  },
+  "3M123": {
+    "departureTime": "1:00 PM",
+    "arrivalTime": "3:00 PM",
+    "departureAirport": "SFO",
+    "arrivalAirport": "LAX",
+    "aircraftType": "ATR 72"
+  },
+  "5Y456": {
+    "departureTime": "4:00 PM",
+    "arrivalTime": "6:00 PM",
+    "departureAirport": "ONT",
+    "arrivalAirport": "LAX",
+    "aircraftType": "Boeing 747"
+  }
+  // add more flight data here
+};
+
 let hover = {
   "Ad": clickable,
   "LAX": lax,
